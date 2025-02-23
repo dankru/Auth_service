0a08040904f28ecc9a71542827bb20bda65fbea8
dankru <noiafugace@gmail.com>
2025-02-23 14:27:42 +0700

    start implementing grpc auth methods

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/dankru/Auth_service/internal/auth"
+	authpb "github.com/dankru/proto-definitions/pkg/auth"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %s", err.Error())
+	}
+
+	s := auth.Server{}
+	grpcServer := grpc.NewServer()
+
+	authpb.RegisterTokenServiceServer(grpcServer, &s)
+	log.Printf("serving")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err.Error())
+	}
+}
