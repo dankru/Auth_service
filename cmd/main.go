60c653e2195f906db33956386f3bb57667f5d5f2
dankru <noiafugace@gmail.com>
2025-02-26 01:09:27 +0700

    use os.getenv

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,11 @@ import (
 	"github.com/dankru/Auth_service/internal/server"
 	"github.com/dankru/Auth_service/internal/service/auth"
 	"log"
+	"os"
 )
 
 func main() {
-	authServer := auth.NewAuthServer([]byte("eqweqwenqwr"))
+	authServer := auth.NewAuthServer([]byte(os.Getenv("HMAC_SECRET")))
 
 	srv := server.NewServer("tcp", ":9000", authServer)
 	if err := srv.Run(); err != nil {
