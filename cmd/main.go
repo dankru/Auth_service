8589583bc100b8eda4f64dedf1dcca1d876e83d2
dankru <noiafugace@gmail.com>
2025-02-28 23:58:38 +0700

    add viper for configs

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"github.com/dankru/Auth_service/internal/server"
 	"github.com/dankru/Auth_service/internal/service/auth"
+	"github.com/spf13/viper"
 	"log"
 	"os"
 )
 
 func main() {
-	authServer := auth.NewAuthServer([]byte(os.Getenv("HMAC_SECRET")))
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 
-	srv := server.NewServer("tcp", ":8081", authServer)
+	authServer := auth.NewAuthServer([]byte(os.Getenv("HMAC_SECRET")))
+	srv := server.NewServer("tcp", viper.GetString("server.port"), authServer)
 	if err := srv.Run(); err != nil {
 		log.Fatal("failed to run: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("../configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
